54f531574e73f14e94b9250ac4cd4259c7ec464a
sam0622 <140025368+sam0622@users.noreply.github.com>
2024-01-05 21:52:21 -0500

    satisfy dependency

--- scripts/global_script.gd
+++ scripts/global_script.gd
@@ -1,6 +1,8 @@
 extends Node
 
 var enemy_can_shoot
+# DO NOT REMOVE BIGGIE_CHEESE.JPEG REQUIRED AS PLACEHOLDER
+var bc = preload("res://biggie_cheese.jpeg")
 
 func _input(event):
 	if event.is_action_pressed("debug_console"):
