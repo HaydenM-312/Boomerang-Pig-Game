5e7a650c4c72f1bbbb2db319ae248bc42eccbc71
sam0622 <sgreenfield517@gmail.com>
2024-01-08 14:24:24 -0500

    Revert "biggie cheese"
    This reverts commit d8c304d3ab84d9255fd80902a52cc587ad0d7a92.

--- scripts/global_script.gd
+++ scripts/global_script.gd
@@ -2,7 +2,7 @@ extends Node
 
 var enemy_can_shoot
 # DO NOT REMOVE BIGGIE_CHEESE.JPEG REQUIRED AS PLACEHOLDER
-var core = preload("res://assets/sprites/core/biggie_cheese.jpeg")
+var bc = preload("res://biggie_cheese.jpeg")
 
 func _input(event):
 	if event.is_action_pressed("debug_console"):
