f867579a8dbc725a1724284d2b15c1c1391e20d9
sam0622 <sgreenfield517@gmail.com>
2024-01-08 14:25:13 -0500

    Revert "satisfy dependency"
    This reverts commit 54f531574e73f14e94b9250ac4cd4259c7ec464a.

--- scripts/global_script.gd
+++ scripts/global_script.gd
@@ -1,8 +1,6 @@
 extends Node
 
 var enemy_can_shoot
-# DO NOT REMOVE BIGGIE_CHEESE.JPEG REQUIRED AS PLACEHOLDER
-var bc = preload("res://biggie_cheese.jpeg")
 
 func _input(event):
 	if event.is_action_pressed("debug_console"):
