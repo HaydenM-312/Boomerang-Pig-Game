2d43532a7fc2ed7612acf155f8f59a887d23820c
Trate318 <137199689+Trate318@users.noreply.github.com>
2024-02-05 14:46:56 -0500

    Delete Boomerang directory

--- Boomerang/Player.gd
+++
@@ -1,8 +0,0 @@
-extends CharacterBody2D
-
-var speed:float = 300.0
-
-func _physics_process(delta):
-	var direction = Input.get_vector("left", "right", "up", "down")
-	velocity = lerp(velocity, direction * speed, .8)
-	move_and_slide()
--- Boomerang/enemy.gd
+++
@@ -1,6 +0,0 @@
-extends CharacterBody2D
-
-@export var detection_range : float
-@export var follow_range : float
-func _process(delta):
-	move_and_slide()
